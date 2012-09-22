28a2282c3d341e498aeab5921e8ad74600e818db
Kevin Wang <born2run5000@yahoo.com>
2012-09-22 00:38:09 -0400

    Add has_many associations to Student.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,6 @@
 class Student < ActiveRecord::Base
   attr_accessible :father_name, :first_name, :id, :last_name, :mother_name, :parent_cell_phone, :parent_email, :parent_home_phone, :preferred_language, :student_cell_phone, :student_email, :year
   has_many :medical_forms
+  has_many :parent_permission_forms
+  has_many :teacher_permission_forms
 end
