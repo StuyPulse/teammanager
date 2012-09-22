f0b254daa80d9f2567923ec1cc5f496d4af1933b
Kevin Wang <born2run5000@yahoo.com>
2012-09-22 12:50:02 -0400

    Add method to Student model that determines whether the student has passed the safety test this year.

--- app/models/student.rb
+++ app/models/student.rb
@@ -8,4 +8,12 @@ class Student < ActiveRecord::Base
   def has_valid_medical_on date
     (self.medical_forms.where 'date > ?', date - 1.year).length > 0
   end
+
+  def passed_safety_test?
+    if Date.today.month < 9
+      (self.safety_tests.where 'date > ?', (Date.new Date.today.year - 1, 9, 1)).length > 0
+    else
+      (self.safety_tests.where 'date > ?', (Date.new Date.today.year, 9, 1)).length > 0
+    end
+  end
 end
