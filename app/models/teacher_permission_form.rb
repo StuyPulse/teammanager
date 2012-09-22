77bc8d9dad863311bc59fe087a66b2193de59a3a
Kevin Wang <born2run5000@yahoo.com>
2012-09-22 00:01:36 -0400

    Generate model teacher permission form.

---
+++ app/models/teacher_permission_form.rb
@@ -0,0 +1,3 @@
+class TeacherPermissionForm < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
---
+++ test/unit/teacher_permission_form_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TeacherPermissionFormTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
