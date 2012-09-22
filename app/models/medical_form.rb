5a3c141f9e5535e6d28750795d0179e53c3d7080
Kevin Wang <born2run5000@yahoo.com>
2012-09-21 23:56:27 -0400

    Add association between medical forms and students.

--- app/models/medical_form.rb
+++ app/models/medical_form.rb
@@ -1,3 +1,4 @@
 class MedicalForm < ActiveRecord::Base
   attr_accessible :date
+  belongs_to :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
   attr_accessible :father_name, :first_name, :id, :last_name, :mother_name, :parent_cell_phone, :parent_email, :parent_home_phone, :preferred_language, :student_cell_phone, :student_email, :year
+  has_many :medical_forms
 end
