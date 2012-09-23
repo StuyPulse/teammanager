c5173ea21d3fa174a92fe9e247431f5e76dbd866
Kevin Wang <born2run5000@yahoo.com>
2012-09-23 01:40:27 -0400

    Add safety tests and medical forms to Student show view.

--- app/models/medical_form.rb
+++ app/models/medical_form.rb
@@ -1,4 +1,8 @@
 class MedicalForm < ActiveRecord::Base
   attr_accessible :date
   belongs_to :student
+
+  def is_valid?
+    self.date > Date.today - 1.year
+  end
 end
--- app/models/safety_test.rb
+++ app/models/safety_test.rb
@@ -1,4 +1,12 @@
 class SafetyTest < ActiveRecord::Base
   attr_accessible :date
   belongs_to :student
+
+  def is_valid?
+    if Date.today.month < 9
+      self.date > Date.new(Date.today.year - 1, 9, 1)
+    else
+      self.date > Date.new(Date.today.year, 9, 1)
+    end
+  end
 end
