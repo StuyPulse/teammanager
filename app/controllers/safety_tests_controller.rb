3513739adebbb08166b9afa1e2dd7d7aef76a099
Kevin Wang <born2run5000@yahoo.com>
2012-09-23 02:06:56 -0400

    Add functionality to add safety tests.

--- app/controllers/safety_tests_controller.rb
+++ app/controllers/safety_tests_controller.rb
@@ -1,8 +1,15 @@
 class SafetyTestsController < ApplicationController
   def new
+    @safety_test = SafetyTest.new
   end
 
   def create
+    @safety_test = Student.find(params[:student_id]).safety_tests.new params[:safety_test]
+    if @safety_test.save
+      redirect_to controller: 'students', action: 'show', id: params[:student_id], message: 'Safety test added successfully.'
+    else
+      render action: 'new'
+    end
   end
 
   def destroy
