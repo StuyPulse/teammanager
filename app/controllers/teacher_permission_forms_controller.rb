5d77daf81397d9f5eed9d64e72e424dc5e959c6f
Kevin Wang <born2run5000@yahoo.com>
2012-09-24 13:15:22 -0400

    Implement everything for teacher permission forms.

--- app/controllers/teacher_permission_forms_controller.rb
+++ app/controllers/teacher_permission_forms_controller.rb
@@ -1,10 +1,21 @@
 class TeacherPermissionFormsController < ApplicationController
   def new
+    @teacher_permission_form = TeacherPermissionForm.new
   end
 
   def create
+    @teacher_permission_form = Student.find(params[:student_id]).teacher_permission_forms.new params[:teacher_permission_form]
+    if @teacher_permission_form.save
+      redirect_to controller: 'students', action: 'show', id: params[:student_id], message: 'Teacher permission form added successfully.'
+    else
+      render action: 'new'
+    end
   end
 
   def destroy
+    form = TeacherPermissionForm.find(params[:id])
+    student_id = form.student_id
+    form.destroy
+    redirect_to controller: 'students', action: 'show', id: student_id, message: 'Teacher permission form deleted succesfully.'
   end
 end
--- app/models/teacher_permission_form.rb
+++ app/models/teacher_permission_form.rb
@@ -1,6 +1,7 @@
 class TeacherPermissionForm < ActiveRecord::Base
   belongs_to :student
   belongs_to :trip
+  attr_accessible :trip_id
   validates :student_id, presence: true
   validates :trip_id, presence: true
 end
