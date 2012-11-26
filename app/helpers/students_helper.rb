6e3c2ed5534ca4db59f5f9cb53f7c5276eea1118
Guest <guest-Lf1lMX@cslab4-16.stuy.edu>
2012-11-26 17:17:20 -0500

    Try adding CSV

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,12 +1,2 @@
-require 'csv'
-
 module StudentsHelper
-  def generate_csv
-    csv_string = CSV.generate do |csv|
-      csv << ["Name", "Email"]
-      Student.order(:last_name).each do |student|
-        CSV << [student.first_name + student.last_name, student.email]
-      end
-    end
-  end
 end
