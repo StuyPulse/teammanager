1a62430a56a0ce93bea3d93e3b7a434c4cbdee10
Guest <guest-Lf1lMX@cslab4-16.stuy.edu>
2012-11-26 17:22:48 -0500

    I HOPE THIS FUCKING WORKS BUT I CANT TEST CAN I?

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Teammanager::Application.routes.draw do
   resources :teacher_permission_forms
   resources :trip_deposits
   resources :trip_fees
+  match '/students/awesome_csv', to: "students#create_csv"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
