3b048431246dbb1cd93de2942b061fdd5ade78fb
PhilippSteinmann <steinmann.philipp@yahoo.com>
2012-12-11 16:26:57 -0500

    Write controller code for team dues

--- app/controllers/team_dues_controller.rb
+++ app/controllers/team_dues_controller.rb
@@ -1,4 +1,24 @@
 class TeamDuesController < ApplicationController
+  def new
+    @team_due = TeamDue.new
+  end
+  
   def create
+    @team_due = Student.find(params[:student_id[).team_dues.new params[:team_due]
+    if @team_due.save
+      flash[:notice] = "Team Due added successfully."
+      redirect_to_controller: "students", action: "show", id: params[:student_id]
+    else
+      flash.now[:alert] = @team_dues.errors.full_messages.join('<br>').html_safe
+      render action: "new"
+    end
+  end
+  
+  def destroy
+    due = TeamDue.find(params[:id])
+    student_id = due.student_id
+    due.destroy
+    flash[:notice] = "Team due deleted successfully."
+    redirect_to_controller: "students", action: "show", id: student_id
   end
 end
