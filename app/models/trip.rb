d638630ab3a6747f58e914655f6b4b2e482c7361
Philipp Steinmann <steinmann.philipp@yahoo.com>
2013-02-01 17:55:18 -0500

    Remove excess whitespace

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -20,12 +20,10 @@ class Trip < ActiveRecord::Base
   has_many :teacher_permission_forms
   has_many :trip_fees
   has_many :trip_deposits
-
   has_many :p_students, through: :parent_permission_forms, source: :student
   has_many :t_students, through: :teacher_permission_forms, source: :student
   has_many :f_students, through: :trip_fees, source: :student
   has_many :d_students, through: :trip_deposits, source: :student
-
   validates :name, presence: true
   validates :start_date, presence: true
   validates :end_date, presence: true
