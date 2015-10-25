94913a92d568e92d17eb23682ab4fc6d720588f1
Justin Kim <yulli@yulli.org>
2015-10-25 18:59:39 -0400

    Fix standard phone number format

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,5 +1,5 @@
 module StudentsHelper
   def formatted_phone_number(phone)
-    "(#{phone[0..2]})-#{phone[3..5]}-#{phone[5..8]}" unless phone.length < 9
+    "(#{phone[0..2]}) #{phone[3..5]}-#{phone[5..8]}" unless phone.length < 9
   end
 end
