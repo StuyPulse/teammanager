131489b8e99cf1f6396f5dae88ea5479c6175784
Justin Kim <yulli@yulli.org>
2015-10-25 18:49:11 -0400

    Normalize phone numbers and display them formatted well

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
   belongs_to :team
+  before_save :format_data
   validates :first_name, :last_name, :graduation_year, :sark, :email, presence: true
   validates :osis, length: { is: 9 },
                    numericality: { greater_than: 0 },
@@ -7,4 +8,13 @@ class Student < ActiveRecord::Base
 
   validates :sark, length: { is: 4 },
                    numericality: { greater_than: 0 }
+
+  private
+
+  def format_data
+    # Normalize phone numbers
+    phone.gsub!(/\D/, '')
+    parent_home_phone.gsub!(/\D/, '')
+    parent_cell_phone.gsub!(/\D/, '')
+  end
 end
