1ed6406e5cc95bfcf33a616fe01009d933844cb7
Nicole Shin <shnnicole12321@yahoo.com>
2015-10-25 16:39:55 -0400

    Add student information to student model

--- db/schema.rb
+++ db/schema.rb
@@ -11,25 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025190850) do
+ActiveRecord::Schema.define(version: 20151025203458) do
 
   create_table "students", force: :cascade do |t|
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
-    t.integer  "graduation_year", null: false
-    t.integer  "osis",            null: false
-    t.integer  "sark",            null: false
-    t.string   "email",           null: false
+    t.string   "first_name",        null: false
+    t.string   "last_name",         null: false
+    t.integer  "graduation_year",   null: false
+    t.integer  "osis",              null: false
+    t.integer  "sark",              null: false
+    t.string   "email",             null: false
     t.string   "phone"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.string   "father_name"
+    t.string   "mother_name"
+    t.string   "parent_email"
+    t.string   "parent_home_phone"
+    t.string   "parent_cell_phone"
   end
 
   add_index "students", ["email"], name: "index_students_on_email"
+  add_index "students", ["father_name"], name: "index_students_on_father_name"
   add_index "students", ["first_name"], name: "index_students_on_first_name"
   add_index "students", ["graduation_year"], name: "index_students_on_graduation_year"
   add_index "students", ["last_name"], name: "index_students_on_last_name"
+  add_index "students", ["mother_name"], name: "index_students_on_mother_name"
   add_index "students", ["osis"], name: "index_students_on_osis", unique: true
+  add_index "students", ["parent_cell_phone"], name: "index_students_on_parent_cell_phone"
+  add_index "students", ["parent_email"], name: "index_students_on_parent_email"
+  add_index "students", ["parent_home_phone"], name: "index_students_on_parent_home_phone"
   add_index "students", ["phone"], name: "index_students_on_phone"
   add_index "students", ["sark"], name: "index_students_on_sark"
 
