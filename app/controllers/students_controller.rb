29e6b08adaee49a17161610f69abecc20eefab68
Nicole Shin <shnnicole12321@yahoo.com>
2015-10-25 16:58:51 -0400

    Add parent info to  Student controller and views

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -70,6 +70,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:first_name, :last_name, :graduation_year, :osis, :sark, :email, :phone)
+      params.require(:student).permit(:first_name, :last_name, :graduation_year, :osis, :sark, :email, :phone, :father_name, :mother_name, :parent_email, :parent_home_phone, :parent_cell_phone)
     end
 end
