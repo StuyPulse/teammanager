719441888ccabae5e424b2360cd6dd57aa83874a
Nicole Shin <shnnicole12321@yahoo.com>
2015-10-25 14:37:36 -0400

    Ensure student osis and sark id are positive

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,6 @@
 class Student < ActiveRecord::Base
   validates :first_name, :last_name, :graduation_year, :osis, :sark, :email, presence: true
+  validates :osis, :sark, numericality: { greater_than: 0 }
   validates :osis, length: { is: 9 }
   validates :sark, length: { is: 4 }
 end
