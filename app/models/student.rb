68ae61ccc25f5afc6aaa08e4333247163fd913bd
Nicole Shin <shnnicole12321@yahoo.com>
2015-10-25 18:22:50 -0400

    Make osis an optional field

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,10 @@
 class Student < ActiveRecord::Base
   belongs_to :team
-  validates :first_name, :last_name, :graduation_year, :osis, :sark, :email, presence: true
-  validates :osis, :sark, numericality: { greater_than: 0 }
-  validates :osis, length: { is: 9 }
-  validates :sark, length: { is: 4 }
+  validates :first_name, :last_name, :graduation_year, :sark, :email, presence: true
+  validates :osis, length: { is: 9 },
+                   numericality: { greater_than: 0 },
+                   allow_blank: true
+
+  validates :sark, length: { is: 4 },
+                   numericality: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151025213949) do
+ActiveRecord::Schema.define(version: 20151025221153) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name",        null: false
     t.string   "last_name",         null: false
     t.integer  "graduation_year",   null: false
-    t.integer  "osis",              null: false
+    t.integer  "osis"
     t.integer  "sark",              null: false
     t.string   "email",             null: false
     t.string   "phone"
