ae444ca086545c96f9a27045fb8dd9080bc5d985
Justin Kim <yulli@yulli.org>
2015-10-25 20:06:41 -0400

    Add flashes; clean up views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,20 @@
 module ApplicationHelper
   def active_class(path)
-    current_page?(path) ? 'active' : ''
+    current_page?(path) ? "active" : ""
+  end
+
+  def bootstrap_class_for_flash(flash_type)
+    case flash_type
+    when "success"
+      "alert-success"
+    when "error"
+      "alert-error"
+    when "alert"
+      "alert-block"
+    when "notice"
+      "alert-info"
+    else
+      flash_type.to_s
+    end
   end
 end
