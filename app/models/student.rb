6748e7c5cf237002dd2a69dc1f63639bf56a442c
Nicole Shin <shnnicole12321@yahoo.com>
2015-12-22 20:10:41 -0500

    Add status of seasonals to export to csv

--- app/models/student.rb
+++ app/models/student.rb
@@ -33,9 +33,10 @@ class Student < ActiveRecord::Base
 
   def self.to_csv
     CSV.generate do |csv|
-      csv << column_names
+      csv << column_names + (SEASONALS.map {|seasonal| "#{seasonal.to_s.pluralize}?"})
       order(:id).each do |student|
-        csv << student.attributes.values_at(*column_names)
+        statuses_of_seasonals = (SEASONALS.map {|seasonal| student.valid_seasonal(seasonal) != nil})
+        csv << student.attributes.values_at(*column_names) + statuses_of_seasonals
       end
     end
   end
