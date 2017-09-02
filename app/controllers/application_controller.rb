b3bb07cb61b007eaf2f6e19c5fe008eb17234fef
Helen Ye <00yhelen@gmail.com>
2017-09-02 17:20:23 -0400

    Remove authorization on devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  after_action :verify_authorized
+  after_action :verify_authorized, unless: :devise_controller?
 end
