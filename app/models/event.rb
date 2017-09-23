e70f056b96d235bf8b234e2aa7a617b605965335
Helen Ye <00yhelen@gmail.com>
2017-09-23 13:05:25 -0400

    Add required validation and exclude fields on admin dashboard"

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,12 @@ class Event < ApplicationRecord
   has_many :services
   has_many :students, through: :services
 
-  validates :name, :date, :valid_year, presence: true, allow_blank: false
+  validates :name, :date, :valid_year, :default_hours, presence: true,
+            allow_blank: false
+
+  rails_admin do
+    create do
+      exclude_fields :services, :students
+    end
+  end
 end
