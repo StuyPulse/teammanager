ad5d9ed6e4ae775644dc9474153940fb176c42c7
Helen Ye <00yhelen@gmail.com>
2017-09-24 17:43:51 -0400

    Normalize and validate phone numbers using phony_rails

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -2,5 +2,7 @@ class Parent < ApplicationRecord
   include Nameable
 
   has_and_belongs_to_many :students
+
   validates :first_name, :last_name, :phone, presence: true, allow_blank: false
+  phony_normalize :phone, default_country_code: 'US'
 end
