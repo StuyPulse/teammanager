ed662b6f6add05aff3cc2f772f33444ad7866bdf
Helen Ye <00yhelen@gmail.com>
2017-09-29 17:35:51 -0400

    Validate team program to be FTC or FRC

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,8 @@ class Team < ApplicationRecord
   has_many :students
   validates :program, :number, :name, presence: true, allow_blank: false
 
+  validates :program, inclusion: { in: %w(FRC FTC) }
+
   def display_name
     "#{program} #{number} #{name}"
   end
--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'program FTC will pass validations' do
+    team = Team.new(name: 'TeamName',
+                    number: '1234')
+    team.program = 'FTC'
+    assert team.save
+  end
+
+  test 'program FRC will pass validations' do
+    team = Team.new(name: 'TeamName',
+                    number: '1234')
+    team.program = 'FRC'
+    assert team.save
+  end
+
+  test 'program other than FTC or FRC will not pass validations' do
+    team = Team.new(name: 'TeamName',
+                    number: '1234')
+    team.program = 'FLL'
+    refute team.save
+    team.program = 'FT'
+    refute team.save
+    team.program = 'fRC'
+    refute team.save
+  end
 end
