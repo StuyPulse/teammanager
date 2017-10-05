d1a3fd0c192794cad6867f028daabb4840172b62
Helen Ye <00yhelen@gmail.com>
2017-10-05 09:19:27 -0400

    Set service hours to event default hours

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,7 +2,13 @@ class Service < ApplicationRecord
   belongs_to :student
   belongs_to :event
 
+  before_save :set_default_hours
+
   rails_admin do
     visible false
+  end
+
+  def set_default_hours
+    self.hours ||= event.default_hours
   end
 end
