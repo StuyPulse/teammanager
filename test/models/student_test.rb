4a1b3f75116275636413c44f6d878813c03e4824
Helen Ye <00yhelen@gmail.com>
2017-10-05 11:08:41 -0400

    Replace repeat test code using setup

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,50 +1,35 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  test "osis with 9 digits will pass validations" do
-    student = Student.new(first_name: "first",
+  setup do
+    @student = Student.new(first_name: "first",
                           last_name: "last",
                           grad_year: 2000,
                           phone: "1234567890",
                           email: "student@test.com")
-    student.parents << Parent.take
-    student.osis = 123456789
-    assert student.valid?
+    @student.parents << Parent.take
+  end
+
+  test "osis with 9 digits will pass validations" do
+    @student.osis = 123456789
+    assert @student.valid?
   end
 
   test "osis with less than 9 digit will not pass validations" do
-    student = Student.new(first_name: "first",
-                          last_name: "last",
-                          grad_year: 2000,
-                          phone: "1234567890",
-                          email: "student@test.com")
-    student.parents << Parent.take
-    student.osis = 12345678
-    refute student.valid?
-    assert_equal [:osis], student.errors.keys
+    @student.osis = 12345678
+    refute @student.valid?
+    assert_equal [:osis], @student.errors.keys
   end
 
   test "osis with greater than 9 digit will not pass validations" do
-    student = Student.new(first_name: "first",
-                          last_name: "last",
-                          grad_year: 2000,
-                          phone: "1234567890",
-                          email: "student@test.com")
-    student.parents << Parent.take
-    student.osis = 1234567890
-    refute student.valid?
-    assert_equal [:osis], student.errors.keys
+    @student.osis = 1234567890
+    refute @student.valid?
+    assert_equal [:osis], @student.errors.keys
   end
 
   test "negative osis with 9 characters will not pass validations" do
-    student = Student.new(first_name: "first",
-                          last_name: "last",
-                          grad_year: 2000,
-                          phone: "1234567890",
-                          email: "student@test.com")
-    student.parents << Parent.take
-    student.osis = -12345678
-    refute student.valid?
-    assert_equal ['cannot be negative'], student.errors.messages[:osis]
+    @student.osis = -12345678
+    refute @student.valid?
+    assert_equal ['cannot be negative'], @student.errors.messages[:osis]
   end
 end
--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,28 +1,24 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  test 'program FTC will pass validations' do
-    team = Team.new(name: 'TeamName',
+  setup do
+    @team = Team.new(name: 'TeamName',
                     number: '1234')
-    team.program = 'FTC'
-    assert team.save
   end
 
-  test 'program FRC will pass validations' do
-    team = Team.new(name: 'TeamName',
-                    number: '1234')
-    team.program = 'FRC'
-    assert team.save
+  test 'programs FTC and FRC will pass validations' do
+    @team.program = 'FTC'
+    assert @team.save
+    @team.program = 'FRC'
+    assert @team.save
   end
 
   test 'program other than FTC or FRC will not pass validations' do
-    team = Team.new(name: 'TeamName',
-                    number: '1234')
-    team.program = 'FLL'
-    refute team.save
-    team.program = 'FT'
-    refute team.save
-    team.program = 'fRC'
-    refute team.save
+    @team.program = 'FLL'
+    refute @team.save
+    @team.program = 'FT'
+    refute @team.save
+    @team.program = 'fRC'
+    refute @team.save
   end
 end
