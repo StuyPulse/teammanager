a5e656d9e1f29e26107b6fc2200c8caacc6ae482
Helen Ye <00yhelen@gmail.com>
2017-10-05 09:19:27 -0400

    Set service hours to event default hours

--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @event = events(:soupkitchen);
+  end
+
+  test "service without hours set to default hours of event" do
+    s = Service.new(student: Student.take,
+                    event: @event)
+    s.save
+    assert s.hours == @event.default_hours
+  end
+
+  test "service with hours set to default hours of event" do
+    s = Service.new(student: Student.take,
+                    event: @event,
+                    hours: 3.0)
+    s.save
+    assert s.hours == 3.0
+  end
 end
