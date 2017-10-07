612966c7255e220cfb06d83bc81bd457502f55c7
Helen Ye <00yhelen@gmail.com>
2017-10-07 16:23:12 -0400

    Make safety test passed if signed

--- app/models/safety_test.rb
+++ app/models/safety_test.rb
@@ -1,4 +1,13 @@
 class SafetyTest < ApplicationRecord
   include Seasonal
   belongs_to :student
+
+  before_validation :make_passed_if_signed
+
+  validates :is_passed, inclusion: { in: [ true ] }, if: :is_signed
+
+  protected
+  def make_passed_if_signed
+    self.is_passed = true if self.is_signed
+  end
 end
--- test/models/safety_test_test.rb
+++ test/models/safety_test_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SafetyTestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @safety_test = safety_tests(:fail)
+  end
+
+  test "a signed test is passed" do
+    @safety_test.is_signed = true
+    assert @safety_test.save
+    assert @safety_test.is_passed
+  end
 end
