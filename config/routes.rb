ac6e5dd46b1ca3e8c79d93092b0664c1245cabb4
Bryan Lai <bryanjl22@hotmail.com>
2018-07-29 12:29:30 -0400

    Disable password authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,9 @@ Rails.application.routes.draw do
   resources :team_dues
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   resources :parents
   resources :teams
   resources :students do
