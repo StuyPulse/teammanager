3f9a2fcc1ff5e4138f340cb7b42b4450ef80a5f3
Bryan Lai <bryanjl22@hotmail.com>
2018-08-12 16:06:39 -0400

    Add ability to link user to its corresponding student
    Also add tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = User.new(email: "test")
+    @user = User.new(email: "test@stuypulse.com")
   end
 
   test "stuypulse.com email will pass validation" do
@@ -13,5 +13,23 @@ class UserTest < ActiveSupport::TestCase
   test "non-stuypulse.com email will not pass validation" do
     @user.email = "justin@kim@ironpulse.com"
     refute @user.valid?
+  end
+
+  test "student and user are already linked" do
+    @user.email = 'nicole.shin@stuypulse.com'
+    @user.student = students(:helen)
+    @user.save
+    assert @user.student == students(:helen)
+  end
+
+  test "student is not linked to the user" do
+    @user.email = "nicole.shin@stuypulse.com"
+    @user.save
+    assert @user.student == students(:nicole)
+  end
+
+  test "student does not exist for the user" do
+    @user.save
+    refute @user.student
   end
 end
