a43a887dcdce3efdbc3653b26c0857ae08d317e3
Bryan Lai <bryanjl22@hotmail.com>
2018-08-12 13:33:39 -0400

    Create partials and fix other issues for editing student profiles

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -51,7 +51,7 @@ class StudentsController < ApplicationController
 
     respond_to do |format|
       if @student.save
-        format.html { redirect_to @student }
+        format.html { redirect_to @student, notice: 'Student was successfully created.' }
         format.json { render :show, status: :created, location: @student }
       else
         format.html { render :new }
@@ -69,7 +69,7 @@ class StudentsController < ApplicationController
         format.html { redirect_to @student, notice: 'Student was successfully updated.' }
         format.json { render :show, status: :ok, location: @student }
       else
-        format.html { render :edit, notice: 'Student was unsuccessfully updated.' }
+        format.html { render :edit }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
