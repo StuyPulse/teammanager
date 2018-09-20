413d1ed1095c2f8e38bd1a9d43804dfef76809a3
Bryan Lai <bryanjl22@hotmail.com>
2018-09-20 17:01:54 -0400

    Create birthday field for students

--- db/migrate/20180920204141_add_birthday_to_students.rb
+++ db/migrate/20180920204141_add_birthday_to_students.rb
@@ -1,6 +1,6 @@
 class AddBirthdayToStudents < ActiveRecord::Migration[5.1]
   def change
-    add_column :students, :birthday, :string
+    add_column :students, :birthday, :date
     add_index :students, :birthday
   end
 end
