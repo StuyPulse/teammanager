c5887e1464d3312fc4ec6b1254cf3971262f04e3
Bryan Lai <bryanjl22@hotmail.com>
2018-09-24 17:42:39 -0400

    Change email to personal_email for multiple files

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -81,6 +81,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:first_name, :last_name, :preferred_name, :grad_year, :osis, :email, :phone, :team_id)
+      params.require(:student).permit(:first_name, :last_name, :preferred_name, :grad_year, :osis, :personal_email, :phone, :team_id)
     end
 end
